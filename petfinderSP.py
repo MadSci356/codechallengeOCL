86deaa7315326eeeaeee7ef262816cf9a40ef1f7
sayam@lenovo-ubuntu <sdpate11@ncsu.edu>
2019-04-17 23:27:17 -0400

    g

--- petfinderSP.py
+++ petfinderSP.py
@@ -2,6 +2,7 @@
 
 
 import argparse
+import requests
 
 """
 petfinderSP is a command line tool to help search for pets of a given type and
@@ -18,7 +19,7 @@ Author: Sayam Patel (sdpate11 AT ncsu.edu)
 Date created: Apr 17 2019
 """
 
-#Process arguments
+#----(1) Process arguments----#
 
 #----Description and help strings----#
 desc = "Simple Pet Finder using Oak City Labs API"
@@ -29,7 +30,24 @@ json_arg_help = "Print out JSON results instead of normal output"
 
 #building arg parse object
 parser = argparse.ArgumentParser(description = desc)
-parser.add_argument("-t", "--type", help=animal_arg_help)
-parser.add_argument("-l", "--location", help=loc_arg_help)
-parser.add_argument("-j", "--json", help=json_arg_help)
+parser.add_argument("-t", "--type", required=True, help=animal_arg_help)
+parser.add_argument("-l", "--location", required=True, help=loc_arg_help)
+parser.add_argument("-j", "--json", help=json_arg_help, action="store_true")
 args = parser.parse_args()
+
+
+#----(2) Request the search----#
+url = 'https://q93x2sq2y7.execute-api.us-east-1.amazonaws.com/staging/pet.find'
+#making the query for the url
+query = {'output': 'full', 'animal': args.type, 'location': args.location}
+
+#TODO: error check on get
+response = requests.get(url, params=query) #getting from url
+
+#----(3) printing the results----#
+
+if (args.json): #raw json format
+        print(response.text)
+else:
+    print("human readable text here.")
+    #TODO: print human readable text
