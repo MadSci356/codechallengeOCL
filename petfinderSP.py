9f50e2f5bcfbb6ab35c3efe89c065e10e828bab7
sayam@lenovo-ubuntu <sdpate11@ncsu.edu>
2019-04-18 02:16:54 -0400

    status code and msg print

--- petfinderSP.py
+++ petfinderSP.py
@@ -3,6 +3,8 @@
 
 import argparse
 import requests
+import json
+import sys
 
 """
 petfinderSP is a command line tool to help search for pets of a given type and
@@ -18,6 +20,8 @@ Basic process (see Readme for details):
 Author: Sayam Patel (sdpate11 AT ncsu.edu)
 Date created: Apr 17 2019
 """
+
+PFAPI_OK = 100 #valid search result status from API
 
 #----(1) Process arguments----#
 
@@ -41,11 +45,23 @@ url = 'https://q93x2sq2y7.execute-api.us-east-1.amazonaws.com/staging/pet.find'
 #making the query for the url
 query = {'output': 'full', 'animal': args.type, 'location': args.location}
 
-#TODO: error check on get
 response = requests.get(url, params=query) #getting from url
 
-#----(3) printing the results----#
+#error check on get (connection error)
+if (response.status_code != requests.codes.ok):
+    response.raise_for_status()
 
+#check status of request
+data = json.loads(response.text) #dictionary
+status = data["petfinder"]["header"]["status"]
+code = int(status["code"])
+if (code != PFAPI_OK): #error from API
+    print(f'Err: {code} {status["message"]}')
+    parser.parse_args("") #TODO: artificially inflict help message??
+    sys.exit(1);
+
+
+#----(3) printing the results----#
 if (args.json): #raw json format
         print(response.text)
 else:
