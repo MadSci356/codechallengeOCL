a9d9591dab5c4010015a12288a9d53805d152a53
sayam@lenovo-ubuntu <sdpate11@ncsu.edu>
2019-04-18 10:54:29 -0400

    working on json to normal formatting

--- petfinderSP.py
+++ petfinderSP.py
@@ -21,16 +21,67 @@ Author: Sayam Patel (sdpate11 AT ncsu.edu)
 Date created: Apr 17 2019
 """
 
+#constants and global vars
 PFAPI_OK = 100 #valid search result status from API
+MISS = "<UNKNOWN>" #value inplace of key:val pair missing from dictionary
+data = {} #dictionary for holding data from reponse
+
+#functions
+"""
+    Prints human readable text from the data dictionary of pet searches.
+   data is assumed to have format:
+   {  ...,
+      "petfinder": {..., lastOffset: "...", pets: [], ...} }
+
+   where each pet in the pets list has format:
+    {...,
+     age: "adult",
+     media: {photos: [{url= "..."}, ...]},
+     name: "whiskers",
+     sex: "M/F",
+     description: "..." }
+    NOTE: dict assumed to be valid (ie petfinder->header->status-> = 100)
+
+    Print format:
+    Name: Name
+        Age: age
+        Sex: M/F
+        Photo: url
+        Description: wrapped with 70 words.
+            new lines indented twice."""
+def json_to_normal():
+
+    #TODO: print search criteria used
+    #TODO: summary of search
+    pets = data["petfinder"]["pets"]
+    for pet in pets:
+        name = get_value(pet, "name")
+        if (name != MISS) {#capitali
+            name = name.capitalize()
+        }
+        #age
+        #Sex
+        #photo url
+        #Desc
+
+
+def get_value(pet, key):
+    value = ""
+    try:
+        value = pet[key]
+    except KeyError: #key missing
+        value = MISS
+    if len(value) == 0: #value is empty
+        value = MISS
+    return value
 
 #----(1) Process arguments----#
 
-#----Description and help strings----#
+#Description and help strings
 desc = "Simple Pet Finder using Oak City Labs API"
 animal_arg_help = "Animal type. eg: dog/cat/rabbit"
 loc_arg_help = "Location to search. eg: Raleigh,NC/Charleston,SC"
 json_arg_help = "Print out JSON results instead of normal output"
-#----END strings----#
 
 #building arg parse object
 parser = argparse.ArgumentParser(description = desc)
@@ -45,13 +96,14 @@ url = 'https://q93x2sq2y7.execute-api.us-east-1.amazonaws.com/staging/pet.find'
 #making the query for the url
 query = {'output': 'full', 'animal': args.type, 'location': args.location}
 
-response = requests.get(url, params=query) #getting from url
+#getting from url
+response = requests.get(url, params=query)
 
 #error check on get (connection error)
 if (response.status_code != requests.codes.ok):
     response.raise_for_status()
 
-#check status of request
+#error check on API returned data
 data = json.loads(response.text) #dictionary
 status = data["petfinder"]["header"]["status"]
 code = int(status["code"])
@@ -63,7 +115,6 @@ if (code != PFAPI_OK): #error from API
 
 #----(3) printing the results----#
 if (args.json): #raw json format
-        print(response.text)
+        print(data)
 else:
-    print("human readable text here.")
-    #TODO: print human readable text
+    json_to_normal(data)
